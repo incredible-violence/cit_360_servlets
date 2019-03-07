3fc2190c4ee4cc916be2bc454dd91f0ec595d178
waabbajak <waabbajak@gmail.com>
2019-03-06 18:41:40 -0700

    working files here

---
+++ ServletDemo/out/artifacts/ServletDemo_war_exploded/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: waabb
+  Date: 03/06/19
+  Time: 5:24 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Welcome</title>
+  </head>
+  <body>
+<main>
+  <h1>Welcome <br>to<br> Simple Sword Servlet Service!</h1>
+  <div id="buttons">
+    <div id="btn1">
+      <a href="${pageContext.request.contextPath}/ServletPrime">Add a Sword</a>
+    </div>
+  </div>
+</main>
+  </body>
+</html>
---
+++ ServletDemo/src/servlet/ServletPrime.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ServletPrime", urlPatterns={"/ServletPrime"})
+public class ServletPrime extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // get data from form
+        String swordName = request.getParameter("swordName");
+        String swordCulture = request.getParameter("swordCulture");
+        String bladeShape = request.getParameter("bladeShape");
+        String swordComments = request.getParameter("swordComments");
+
+        // display on web page
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        out.println("<html><head><link rel='stylesheet' href='formTable.css'></head><body>");
+        out.println("<h1>You Input The Following:</h1>");
+        out.println("<table>");
+        out.println("<tbody>");
+        out.println("<tr><th>Sword Name</th><td>" + swordName + "</td></tr>");
+        out.println("<tr><th>Sword Culture</th><td>" + swordCulture + "</td></tr>");
+        out.println("<tr><th>Blade Shape</th><td>" + bladeShape + "</td></tr>");
+        out.println("<tr><th>Comments</th><td>" + swordComments + "</td></tr>");
+        out.println("</tbody></table>");
+        out.println("</body></html>");
+
+        // write data to a cookie?
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // user should never see this
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("What are you doing here!?");
+        response.sendRedirect("/ServletDemo_war_exploded/swordInput.html");
+    }
+}
